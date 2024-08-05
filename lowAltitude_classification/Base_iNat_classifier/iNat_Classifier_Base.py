ba0f2063b8061511bdf1bb40a3b733c0e34769f1
Damien LaRocque <phicoltan@gmail.com>
2024-08-05 18:51:25 -0400

    Run classifier on the server
    	- Add slurm scripts
    	- Update requirements
    	- Update json file

--- lowAltitude_classification/Base_iNat_classifier/iNat_Classifier_Base.py
+++ lowAltitude_classification/Base_iNat_classifier/iNat_Classifier_Base.py
@@ -3,8 +3,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from albumentations import (
-    RandomResizedCrop, HorizontalFlip, Normalize, SmallestMaxSize,
-    Compose, CenterCrop
+    RandomResizedCrop,
+    HorizontalFlip,
+    Normalize,
+    SmallestMaxSize,
+    Compose,
+    CenterCrop,
 )
 from albumentations.pytorch import ToTensorV2
 from sklearn.model_selection import StratifiedKFold
@@ -12,6 +16,8 @@ from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader, Subset
 from torchvision.datasets import ImageFolder
 from transformers import AutoImageProcessor, AutoModelForImageClassification
+import json
+from pathlib import Path
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -21,35 +27,47 @@ output_file_path = "/home/kamyar/PycharmProjects/droneSegmentation/lowAltitude_c
 dataset = ImageFolder(root=data_folder)
 label_to_id = dataset.class_to_idx
 
-with open(output_file_path, 'w') as file:
+with open(output_file_path, "w") as file:
     for label, idx in label_to_id.items():
         file.write(f"{label}: {idx}\n")
 
-model_name = 'facebook/dinov2-large-imagenet1k-1-layer'
+json_path = Path(output_file_path).with_suffix(".json")
+with open(json_path, "w") as f:
+    json.dump(label_to_id, f, indent=2, sort_keys=True)
+
+model_name = "facebook/dinov2-large-imagenet1k-1-layer"
 processor = AutoImageProcessor.from_pretrained(model_name)
-model = AutoModelForImageClassification.from_pretrained(model_name, ignore_mismatched_sizes=True)
+model = AutoModelForImageClassification.from_pretrained(
+    model_name, ignore_mismatched_sizes=True
+)
 
 mean = processor.image_mean
 std = processor.image_std
 
-train_transform = Compose([
-    SmallestMaxSize(max_size=256),
-    RandomResizedCrop(height=256, width=256, scale=(0.4, 1.0), ratio=(0.75, 1.3333)),
-    HorizontalFlip(p=0.5),
-    Normalize(mean=mean, std=std),
-    ToTensorV2()
-])
+train_transform = Compose(
+    [
+        SmallestMaxSize(max_size=256),
+        RandomResizedCrop(
+            height=256, width=256, scale=(0.4, 1.0), ratio=(0.75, 1.3333)
+        ),
+        HorizontalFlip(p=0.5),
+        Normalize(mean=mean, std=std),
+        ToTensorV2(),
+    ]
+)
 
-test_transform = Compose([
-    SmallestMaxSize(max_size=256),
-    CenterCrop(height=256, width=256),
-    Normalize(mean=mean, std=std),
-    ToTensorV2()
-])
+test_transform = Compose(
+    [
+        SmallestMaxSize(max_size=256),
+        CenterCrop(height=256, width=256),
+        Normalize(mean=mean, std=std),
+        ToTensorV2(),
+    ]
+)
 
 
 def albumentations_transform(dataset, transform):
-    dataset.transform = lambda img: transform(image=np.array(img))['image']
+    dataset.transform = lambda img: transform(image=np.array(img))["image"]
     return dataset
 
 
@@ -67,7 +85,10 @@ for fold, (train_idx, val_idx) in enumerate(kf.split(dataset, dataset.targets)):
     train_loader = DataLoader(train_subset, shuffle=True, batch_size=16, num_workers=16)
     val_loader = DataLoader(val_subset, batch_size=16, shuffle=False, num_workers=16)
 
-    model = AutoModelForImageClassification.from_pretrained(model_name, ignore_mismatched_sizes=True)
+    model = AutoModelForImageClassification.from_pretrained(
+        model_name,
+        ignore_mismatched_sizes=True,
+    )
     model.classifier = nn.Linear(2048, len(label_to_id)).to(device)
     model = model.to(device)
 
@@ -97,10 +118,13 @@ for fold, (train_idx, val_idx) in enumerate(kf.split(dataset, dataset.targets)):
 
             loss_accumulated_train += loss.item() * y_batch.size(0)
             total_samples_train += y_batch.size(0)
-            correct_predictions_train += (torch.argmax(logits, dim=1) == y_batch).sum().item()
+            correct_predictions_train += (
+                (torch.argmax(logits, dim=1) == y_batch).sum().item()
+            )
 
         print(
-            f'Epoch {epoch + 1} - Training Loss: {loss_accumulated_train / total_samples_train:.4f} Accuracy: {correct_predictions_train / total_samples_train:.4f}')
+            f"Epoch {epoch + 1} - Training Loss: {loss_accumulated_train / total_samples_train:.4f} Accuracy: {correct_predictions_train / total_samples_train:.4f}"
+        )
         scheduler.step()
 
         # Validation
@@ -118,20 +142,27 @@ for fold, (train_idx, val_idx) in enumerate(kf.split(dataset, dataset.targets)):
 
                 loss_accumulated_valid += loss.item() * y_batch.size(0)
                 total_samples_valid += y_batch.size(0)
-                correct_predictions_valid += (torch.argmax(logits, dim=1) == y_batch).sum().item()
+                correct_predictions_valid += (
+                    (torch.argmax(logits, dim=1) == y_batch).sum().item()
+                )
 
             accuracy_valid = correct_predictions_valid / total_samples_valid
-            print(f'Validation Loss: {loss_accumulated_valid / total_samples_valid:.4f} Accuracy: {accuracy_valid:.4f}')
+            print(
+                f"Validation Loss: {loss_accumulated_valid / total_samples_valid:.4f} Accuracy: {accuracy_valid:.4f}"
+            )
 
             if accuracy_valid > best_accuracy:
                 best_accuracy = accuracy_valid
                 best_epoch = epoch
                 best_model_weights = model.state_dict()
                 pth_name = f"0{fold + 1}_base_{best_epoch + 1}e_acc{100 * best_accuracy:2.0f}.pth"
-                torch.save(best_model_weights,
-                           f'/home/kamyar/PycharmProjects/droneSegmentation/lowAltitude_classification/checkpoints/{pth_name}')
+                torch.save(
+                    best_model_weights,
+                    f"/home/kamyar/PycharmProjects/droneSegmentation/lowAltitude_classification/checkpoints/{pth_name}",
+                )
                 print(
-                    f"Best model weights saved for fold {fold + 1} at epoch {best_epoch + 1} with accuracy {best_accuracy:.4f}")
+                    f"Best model weights saved for fold {fold + 1} at epoch {best_epoch + 1} with accuracy {best_accuracy:.4f}"
+                )
 
     fold_accuracies.append(best_accuracy)
 
