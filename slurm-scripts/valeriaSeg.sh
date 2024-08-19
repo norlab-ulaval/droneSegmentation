3f99abcd36bf9ec0666b898d2cb15e9e28a418a4
William Guimont-Martin <wil9876543210@hotmail.com>
2024-08-19 11:12:10 -0400

    fixing valeria scripts

--- slurm-scripts/valeriaSeg.sh
+++ slurm-scripts/valeriaSeg.sh
@@ -10,23 +10,13 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --reservation=wigum_9
 
-module load python/3.10.13
-module load cuda/12.2
-module load qt
-module load geos
-module load llvm
-module load gcc
-module load opencv
-module load scipy-stack
-module load openblas
-
 cd $SLURM_TMPDIR || exit 1
 cp -r ~/detectron2 ./
 cp -r ~/droneSegmentation ./
 
 module load python/3.11.5
 module load cuda/11.7
-module load opencv/4.9.0
+module load opencv
 
 s3cmd sync s3://annotated $SLURM_TMPDIR
 
@@ -34,13 +24,13 @@ virtualenv --no-download venv
 source venv/bin/activate
 pip install --no-index --upgrade pip
 
-cd $SLURM_TMPDIR/detectron2 || exit 1
-pip install -e . --no-index
-
 cd $SLURM_TMPDIR/droneSegmentation/lowAltitude_segmentation/Mask2Former || exit 1
 pip install -r requirements.txt --no-index
 
-cd mask2former/modeling/pixel_decoder/ops || exit 1
+cd $SLURM_TMPDIR/detectron2 || exit 1
+pip install -e . --no-index
+
+cd $SLURM_TMPDIR/droneSegmentation/lowAltitude_segmentation/Mask2Former/mask2former/modeling/pixel_decoder/ops || exit 1
 sh make.sh
 
 cd $SLURM_TMPDIR/droneSegmentation/ || exit 1
