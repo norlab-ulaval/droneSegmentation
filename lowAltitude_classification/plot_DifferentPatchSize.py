2fd68954863fdd53bbc2c7bea04f16378dc6d13a
kamyarNsr <thekamyar.nasiri@gmail.com>
2024-08-22 11:33:54 -0400

    num_workers

--- lowAltitude_classification/plot_DifferentPatchSize.py
+++ lowAltitude_classification/plot_DifferentPatchSize.py
@@ -6,16 +6,14 @@ df_val = pd.read_csv('lowAltitude_classification/Result_Val_DifferentPatcheSize/
 df_test = pd.read_csv('lowAltitude_classification/Result_Test_DifferentPatcheSize/Result_Test_DifferentPatcheSize.csv')
 
 # Columns to exclude0
-exclude_columns = ['precision', 'recall', 'mIoU']
-
-# Generate the combined column labels
-combined_columns = [f'{row[df_val.columns[1]]}_{row[df_val.columns[2]]}' for _, row in df_val.iterrows()]
+exclude_columns = ['mIoU']
+combined_columns = [f'{row[df_val.columns[0]]}_{row[df_val.columns[1]]}' for _, row in df_val.iterrows()]
 
 fig, ax = plt.subplots(figsize=(10, 6))
 width = 0.2
 
 x = np.arange(len(df_val.index))
-filtered_columns_val = [col for col in df_val.columns[3:] if col not in exclude_columns]
+filtered_columns_val = [col for col in df_val.columns[2:] if col not in exclude_columns]
 for i, column in enumerate(filtered_columns_val):
     ax.bar(x + i * width, df_val[column], width, label=f'Val - {column}')
 
@@ -27,10 +25,11 @@ ax.legend()
 ax.grid(True)
 # plt.tight_layout()
 plt.title('(A) Validation Data')
-plt.savefig(fname='lowAltitude_classification/Result_Val_DifferentPatcheSize/Validation_DifferentPatchSizes.png')
+plt.show()
+# plt.savefig(fname='lowAltitude_classification/Result_Val_DifferentPatcheSize/Validation_DifferentPatchSizes.png')
 
 fig, ax = plt.subplots(figsize=(10, 6))
-filtered_columns_test = [col for col in df_test.columns[3:] if col not in exclude_columns]
+filtered_columns_test = [col for col in df_test.columns[2:] if col not in exclude_columns]
 for i, column in enumerate(filtered_columns_test):
     ax.bar(x + i * width, df_test[column], width, label=f'Test - {column}')
 
@@ -42,4 +41,5 @@ ax.legend()
 ax.grid(True)
 # plt.tight_layout()
 plt.title('(B) Test Data')
-plt.savefig(fname='lowAltitude_classification/Result_Test_DifferentPatcheSize/Test_DifferentPatchSizes.png')
+plt.show()
+# plt.savefig(fname='lowAltitude_classification/Result_Test_DifferentPatcheSize/Test_DifferentPatchSizes.png')
--- lowAltitude_classification/view_pseudo_labels.py
+++ lowAltitude_classification/view_pseudo_labels.py
@@ -1,8 +1,8 @@
 import os
 import cv2
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 def load_images_from_folder(folder, color_mode=cv2.IMREAD_GRAYSCALE):
     images = []
@@ -52,7 +52,7 @@ def compute_f1_score(prediction, target, num_classes, ignored_classes, epsilon=1
         f1_scores.append(f1)
     return np.mean(f1_scores) if f1_scores else 0, precision, recall
 
-def evaluate_segmentation(pred_folder, target_folder, ignored_classes):
+def evaluate_segmentation(query_filename, pred_folder, target_folder, ignored_classes):
     pred_images, pred_filenames = load_images_from_folder(pred_folder)
     target_images, _ = load_images_from_folder(target_folder)
 
@@ -63,6 +63,9 @@ def evaluate_segmentation(pred_folder, target_folder, ignored_classes):
     total_recall = 0
 
     for pred, target, filename in zip(pred_images, target_images, pred_filenames):
+        if filename != query_filename:
+            continue
+        print(filename, query_filename)
         iou = compute_iou(
             pred, target, num_classes=26, ignored_classes=ignored_classes
         )
@@ -79,24 +82,24 @@ def evaluate_segmentation(pred_folder, target_folder, ignored_classes):
         total_precision += precision
         total_recall += recall
 
-        # # # Plot prediction and target side by side
-        # plt.figure(figsize=(10, 5))
-        #
-        # # Plot the target image
-        # plt.subplot(1, 2, 1)
-        # plt.imshow(target, cmap='gray')
-        # plt.title(f"Ground Truth\nIoU: {iou:.2f}, Accuracy: {accuracy:.2f}\nF1 Score: {f1_score:.2f}, Precision: {precision:.2f}, Recall: {recall:.2f}")
-        # plt.axis('off')
-        #
-        # # Plot the prediction image
-        # plt.subplot(1, 2, 2)
-        # plt.imshow(pred, cmap='gray')
-        # plt.title(f"Prediction\nIoU: {iou:.2f}, Accuracy: {accuracy:.2f}\nF1 Score: {f1_score:.2f}, Precision: {precision:.2f}, Recall: {recall:.2f}")
-        # plt.axis('off')
-        #
-        # # Show the plots
-        # plt.suptitle(f"Image: {filename}", fontsize=16)
-        # plt.show()
+        # Plot prediction and target side by side
+        plt.figure(figsize=(10, 5))
+
+        # Plot the target image
+        plt.subplot(1, 2, 1)
+        plt.imshow(target, cmap='gray')
+        plt.title(f"Ground Truth\nIoU: {iou:.2f}, Accuracy: {accuracy:.2f}\nF1 Score: {f1_score:.2f}, Precision: {precision:.2f}, Recall: {recall:.2f}")
+        plt.axis('off')
+
+        # Plot the prediction image
+        plt.subplot(1, 2, 2)
+        plt.imshow(pred, cmap='gray')
+        plt.title(f"Prediction\nIoU: {iou:.2f}, Accuracy: {accuracy:.2f}\nF1 Score: {f1_score:.2f}, Precision: {precision:.2f}, Recall: {recall:.2f}")
+        plt.axis('off')
+
+        # Show the plots
+        plt.suptitle(f"Image: {filename}", fontsize=16)
+        plt.show()
 
     avg_iou = total_iou / len(pred_images)
     avg_accuracy = total_accuracy / len(pred_images)
@@ -109,59 +112,48 @@ def evaluate_segmentation(pred_folder, target_folder, ignored_classes):
 if __name__ == "__main__":
     ignored_classes = [1]
 
-    parent_preds_folder = '/home/kamyar/Documents/Test_Annotated_Predictions/CENTER/52_Final_5e'
+    parent_preds_folder = '/home/kamyar/Documents/Test_Annotated_Predictions/52_Final_5e/'
     target_folder = '/home/kamyar/Documents/Test_Annotated_masks'
+    image_query = '2024-06-06-023928-5-Zec-Batiscan-4000x4000-DJI-FC7303-patch-1.png'
+    print(Path(image_query).exists())
 
     results = []
 
-    for subdir in os.listdir(parent_preds_folder):
-        # if subdir == 'CENTER' or subdir == 'DifferentPatchSize':
-        #     continue
+    if os.path.isdir(parent_preds_folder):
+        avg_iou, avg_accuracy, avg_f1_score, avg_precision, avg_recall = evaluate_segmentation(
+            image_query, parent_preds_folder, target_folder, ignored_classes
+        )
+        print(f'mIoU: {avg_iou}, pAccuracy: {avg_accuracy}, F1 Score: {avg_f1_score}, Precision: {avg_precision}, Recall: {avg_recall}')
 
-        # print(subdir)
-
-        subdir_path = os.path.join(parent_preds_folder, subdir)
-
-        if os.path.isdir(subdir_path):
-            avg_iou, avg_accuracy, avg_f1_score, avg_precision, avg_recall = evaluate_segmentation(
-                subdir_path, target_folder, ignored_classes
-            )
-            print(f'mIoU: {avg_iou}, pAccuracy: {avg_accuracy}, F1 Score: {avg_f1_score}, Precision: {avg_precision}, Recall: {avg_recall}')
-            # results.append({
-            #     "Experiment": f"experiment {subdir}",
-            #     "mIoU": f'{avg_iou:.4f}',
-            #     "pAcc": f'{avg_accuracy:.4f}',
-            #     "F1": f'{avg_f1_score:.4f}',
-            # })
 
             ####################### Different Patch sizes and overlaps
             # results.append({
-            #     # "Experiment": f"experiment {idx}",
-            #     "Patch Size": subdir.split('_')[0],
-            #     "Overlap": subdir.split('_')[1],
-            #     # "precision": f'{avg_precision:.4f}',
-            #     # "recall": f'{avg_recall:.4f}',
+            #     "Experiment": f"experiment {idx}",
+            #     "Patch Size": pred_folder_name.split('_')[0],
+            #     "Overlap": pred_folder_name.split('_')[1],
+            #     "precision": f'{avg_precision:.4f}',
+            #     "recall": f'{avg_recall:.4f}',
             #     "mIoU": f'{avg_iou:.4f}',
             #     "pAcc": f'{avg_accuracy:.4f}',
             #     "F1": f'{avg_f1_score:.4f}',
             # })
 
             ######################## Different center assignment sizes
-            results.append({
-                # "Experiment": f"experiment {idx}",
-                "Central Size": subdir.split('_')[0],
-                "Patch Size": subdir.split('_')[1],
-                "Overlap": subdir.split('_')[2],
-
-                # "precision": f'{avg_precision:.4f}',
-                # "recall": f'{avg_recall:.4f}',
-                "mIoU": f'{avg_iou:.4f}',
-                "pAcc": f'{avg_accuracy:.4f}',
-                "F1": f'{avg_f1_score:.4f}',
-            })
+    #         results.append({
+    #             # "Experiment": f"experiment {idx}",
+    #             "Central Size": pred_folder_name.split('_')[0],
+    #             "Patch Size": pred_folder_name.split('_')[1],
+    #             "Overlap": pred_folder_name.split('_')[2],
     #
-    df = pd.DataFrame(results)
-    df = df.sort_values(by=["Patch Size"])
-
-    df.to_csv("lowAltitude_classification/Result_Test_CENTER/Result_Test_CENTER.csv",
-              index=False)
+    #             # "precision": f'{avg_precision:.4f}',
+    #             # "recall": f'{avg_recall:.4f}',
+    #             "mIoU": f'{avg_iou:.4f}',
+    #             "pAcc": f'{avg_accuracy:.4f}',
+    #             "F1": f'{avg_f1_score:.4f}',
+    #         })
+    #
+    # df = pd.DataFrame(results)
+    # df = df.sort_values(by=["Central Size"])
+    #
+    # df.to_csv("lowAltitude_classification/Result_Val_CENTER/Result_Val_CENTER.csv",
+    #           index=False)
