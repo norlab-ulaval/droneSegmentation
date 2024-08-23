33f470e7209882ad71d48f4ac12e1eec37544faa
William Guimont-Martin <wil9876543210@hotmail.com>
2024-08-23 10:26:01 -0400

    wip

--- slurm-scripts/start_jobs_valeria.sh
+++ slurm-scripts/start_jobs_valeria.sh
@@ -10,7 +10,12 @@ sjm run valeria sjmValeria.sh NAME=resnet50_dice CONFIG=M2F_ResNet50_ClassMaskDi
 sjm run valeria sjmValeria.sh NAME=resnet50_colaug CONFIG=M2F_ResNet50_colorAugs.yaml
 sjm run valeria sjmValeria.sh NAME=resnet50_crop CONFIG=M2F_ResNet50_Crop_512.yaml
 
-sjm run valeria sjmValeria.sh NAME=swin_train CONFIG=swin/M2F_Swin_Large_MaxTrainSize_1024.yaml
-sjm run valeria sjmValeria.sh NAME=swin_ssd CONFIG=swin/M2F_Swin_Large_SSD_default.yaml
-sjm run valeria sjmValeria.sh NAME=resnet50_train CONFIG=M2F_ResNet50_MaxTrainSize_1024.yaml
-sjm run valeria sjmValeria.sh NAME=resnet50_ssd CONFIG=M2F_ResNet50_SSD_default.yaml
+
+# WGM
+#sjm run valeria sjmValeria.sh NAME=swin_train CONFIG=swin/M2F_Swin_Large_MaxTrainSize_1024.yaml
+# TODO
+#sjm run valeria sjmValeria.sh NAME=swin_ssd CONFIG=swin/M2F_Swin_Large_SSD_default.yaml
+# DAD
+#sjm run valeria sjmValeria.sh NAME=resnet50_train CONFIG=M2F_ResNet50_MaxTrainSize_1024.yaml
+# TODO
+#sjm run valeria sjmValeria.sh NAME=resnet50_ssd CONFIG=M2F_ResNet50_SSD_default.yaml
