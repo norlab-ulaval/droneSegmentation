ad9c20311190751b9bb9b69aa0d854e3fdc8d213
kamyarNsr <thekamyar.nasiri@gmail.com>
2024-08-31 12:54:58 -0400

    patch sizes

--- lowAltitude_classification/plot_EntireVsCenter.py
+++ lowAltitude_classification/plot_EntireVsCenter.py
@@ -25,14 +25,17 @@ filtered_val.F1 *= 100
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
 
-bars1 = ax1.bar(filtered_val['Central Size'], filtered_val.F1, color='teal', width=16, edgecolor='black')
+central_sizes = filtered_val['Central Size'].unique()
+central_sizes_labels = [str(size) for size in central_sizes]
+
+bars1 = ax1.bar(central_sizes_labels, filtered_val.F1, color='teal', width=0.4, edgecolor='black')
 ax1.set_xlabel('Central Window Size')
 ax1.set_ylabel('F1 score (\%)')
 ax1.grid(True, linestyle='--', linewidth=0.5, alpha=0.7)
 ax1.xaxis.set_label_coords(0.5, -0.12)
 ax1.yaxis.set_label_coords(-0.20, 0.5)
 ax1.set_title(r'$D_{val}^{drone}$', color='b')
-ax1.set_xticks(filtered_val['Central Size'])
+# ax1.set_xticks(filtered_val['Central Size'])
 ax1.set_ylim([filtered_val.F1.min() - 1, filtered_val.F1.max() + 1])
 ax1.yaxis.set_major_locator(plt.MultipleLocator(0.5))
 
@@ -40,12 +43,12 @@ for bar in bars1:
     yval = bar.get_height()
     ax1.text(bar.get_x() + bar.get_width()/2, yval + 0.07, f'${round(yval, 2)}$', fontsize='x-small', color='darkgreen', ha='center', va='bottom')
 
-bars2 = ax2.bar(filtered_test['Central Size'], filtered_test.F1, color='teal', width=16, edgecolor='black')
+bars2 = ax2.bar(central_sizes_labels, filtered_test.F1, color='teal', width=0.4, edgecolor='black')
 ax2.set_xlabel('Central Window Size')
 ax2.grid(True, linestyle='--', linewidth=0.7, alpha=0.7)
 ax2.xaxis.set_label_coords(0.5, -0.12)
 ax2.set_title(r'$D_{test}^{drone}$', color='b')
-ax2.set_xticks(filtered_test['Central Size'])
+# ax2.set_xticks(filtered_test['Central Size'])
 ax2.set_ylim([filtered_test.F1.min() - 1, filtered_test.F1.max() + 1])
 ax2.yaxis.set_major_locator(plt.MultipleLocator(0.5))
 
