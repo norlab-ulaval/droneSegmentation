022e2d2b4189f84c6c6a159736c5f77237f4b33e
kamyarNsr <thekamyar.nasiri@gmail.com>
2024-08-31 12:54:58 -0400

    patch sizes

--- lowAltitude_classification/plot_DifferentPatchSize.py
+++ lowAltitude_classification/plot_DifferentPatchSize.py
@@ -21,17 +21,21 @@ df_test_filtered.F1 *= 100
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
 
-ax1.plot(df_val_filtered.patch_size, df_val_filtered.F1, marker='o', linestyle='-', color='forestgreen')
-ax1.set_xlabel('Average Number of Voters')
+patch_sizes = df_test_filtered['patch_size'].unique()
+patch_size_labels = [str(size) for size in patch_sizes]
+
+ax1.bar(patch_size_labels, df_val_filtered.F1, color='slateblue', width=0.3, edgecolor='black')
+ax1.set_xlabel('Patch Size')
 ax1.set_ylabel('F1 score (\%)')
-ax1.grid(True)
+ax1.grid(True, linestyle='--', linewidth=0.7, alpha=0.7)
 ax1.xaxis.set_label_coords(0.5, -0.12)
-ax1.yaxis.set_label_coords(-0.20, 0.5)
+ax1.yaxis.set_label_coords(-0.17, 0.5)
 ax1.set_title(r'$D_{val}^{drone}$', color='blue')
 
-ax2.plot(df_test_filtered.patch_size, df_test_filtered.F1, marker='o', linestyle='-', color='forestgreen')
-ax2.set_xlabel('Average Number of Voters')
-ax2.grid(True)
+
+ax2.bar(patch_size_labels, df_test_filtered.F1, color='slateblue', width=0.3, edgecolor='black')
+ax2.set_xlabel('Patch Size')
+ax2.grid(True, linestyle='--', linewidth=0.7, alpha=0.7)
 ax2.xaxis.set_label_coords(0.5, -0.12)
 ax2.set_title(r'$D_{test}^{drone}$', color='blue')
 
