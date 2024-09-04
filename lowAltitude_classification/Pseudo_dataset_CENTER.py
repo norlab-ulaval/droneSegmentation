726938e3604f884a98e9be8589a6124741f59d88
kamyarNsr <thekamyar.nasiri@gmail.com>
2024-09-04 10:02:50 -0400

    csvs

--- lowAltitude_classification/Pseudo_dataset_CENTER.py
+++ lowAltitude_classification/Pseudo_dataset_CENTER.py
@@ -27,146 +27,146 @@ transform = Compose([
     ToTensorV2()
 ])
 
-patch_sizes = [144, 184, 256, 320]
+patch_sizes = [184]
 overlaps = [0.85]
 
 weight_file_path = Path('/home/kamyar/Documents/Best_classifier_Weight/52_Final_time2024-08-15_best_5e_acc94.pth')
-image_folder = '/home/kamyar/Documents/Train-val_Annotated'
+image_folder = '/home/kamyar/Documents/Test_Annotated'
 
-# csv_filename = 'lowAltitude_classification/results/phase2/patchsize/val/phase2-val-patchsize.csv'
-# file_exists = os.path.isfile(csv_filename)
+csv_filename = 'lowAltitude_classification/results/New_phase_2/stride/test/phase2-test-patch.csv'
+file_exists = os.path.isfile(csv_filename)
+#
+with open(csv_filename, mode='a', newline='') as file:
+    writer = csv.writer(file)
+    if not file_exists or os.stat(csv_filename).st_size == 0:
+        writer.writerow(["Central Size", "Patch Size", "Step Size", "Avg_Voters", "Pad Size"])
 
-# with open(csv_filename, mode='a', newline='') as file:
-#     writer = csv.writer(file)
-#     if not file_exists or os.stat(csv_filename).st_size == 0:
-#         writer.writerow(["Central Size", "Patch Size", "Step Size", "Avg_Voters", "Pad Size"])
+    model.load_state_dict(torch.load(weight_file_path))
+    model.eval()
 
-model.load_state_dict(torch.load(weight_file_path))
-model.eval()
+    filename = weight_file_path.name
+    output_folder_name = f"{filename.split('_')[0]}_{filename.split('_')[1]}_{filename.split('_')[4]}"
+    output_folder = Path(f'/home/kamyar/Documents/Test_Annotated_Predictions/DifferentStrideSize_center184/{output_folder_name}')
+    output_folder.mkdir(exist_ok=True, parents=True)
 
-filename = weight_file_path.name
-output_folder_name = f"{filename.split('_')[0]}_{filename.split('_')[1]}_{filename.split('_')[4]}"
-output_folder = Path(f'/home/kamyar/Documents/Train-val_Annotated_Predictions/DifferentPatchSize/{output_folder_name}')
-output_folder.mkdir(exist_ok=True, parents=True)
+    for patch_size in patch_sizes:
+        central_window_size = patch_size
+        central_offset = (patch_size - central_window_size) // 2
+        x_offsets, y_offsets = np.meshgrid(
+            np.arange(central_offset, central_offset + central_window_size),
+            np.arange(central_offset, central_offset + central_window_size)
+        )
+        offsets = np.stack([x_offsets, y_offsets], axis=-1).reshape(-1, 2)
 
-for patch_size in patch_sizes:
-    central_window_size = 184
-    central_offset = (patch_size - central_window_size) // 2
-    x_offsets, y_offsets = np.meshgrid(
-        np.arange(central_offset, central_offset + central_window_size),
-        np.arange(central_offset, central_offset + central_window_size)
-    )
-    offsets = np.stack([x_offsets, y_offsets], axis=-1).reshape(-1, 2)
+        for overlap in overlaps:
+            for padding in [patch_size]:
+            # padding = 0
+            #     step_size = int(patch_size * (1 - overlap))
 
-    for overlap in overlaps:
-        for padding in [patch_size]:
-        # padding = 0
-        # step_size = int(patch_size * (1 - overlap))
+                ###################################################################
+                # STEP SIZE DEFINITION OTHER THAN overlap=0.85
+                for step_size in [23, 27, 46, 92, 184]:
+                # for step_size in [27]:
+                # step_size = 24                  ###
+                    batch_size = 256
 
-            ###################################################################
-            # STEP SIZE DEFINITION OTHER THAN overlap=0.85
-            # for step_size in [16, 24, 27, 48, 96]:
-            for step_size in [27]:
-            # step_size = 24                  ###
-                batch_size = 256
+                    total_votings = 0
+                    total_pixels = 0
+                    image_count = 0
 
-                total_votings = 0
-                total_pixels = 0
-                image_count = 0
+                    for image_file in os.listdir(image_folder):
+                        if image_file.endswith(('.jpg', '.JPG', '.png')):
+                            begin_time = time.perf_counter()
+                            image_path = os.path.join(image_folder, image_file)
+                            image = Image.open(image_path)
+                            # image = image.transform() # tx=0, ty=padding
+                            image_np = np.array(image)
+                            transformed = transform(image=image_np)
+                            image_tensor = transformed['image'].to(device)
 
-                for image_file in os.listdir(image_folder):
-                    if image_file.endswith(('.jpg', '.JPG', '.png')):
-                        begin_time = time.perf_counter()
-                        image_path = os.path.join(image_folder, image_file)
-                        image = Image.open(image_path)
-                        # image = image.transform() # tx=0, ty=padding
-                        image_np = np.array(image)
-                        transformed = transform(image=image_np)
-                        image_tensor = transformed['image'].to(device)
+                            image_tensor_padded = torch.nn.functional.pad(
+                                image_tensor, (padding, padding, padding, padding), 'constant', value=0
+                            )
 
-                        image_tensor_padded = torch.nn.functional.pad(
-                            image_tensor, (padding, padding, padding, padding), 'constant', value=0
-                        )
+                            width, height = image.size
+                            padded_width = width + 2 * padding
+                            padded_height = height + 2 * padding
 
-                        width, height = image.size
-                        padded_width = width + 2 * padding
-                        padded_height = height + 2 * padding
+                            pixel_predictions = np.zeros((height, width, num_classes), dtype=np.longlong)
+                            patches = []
+                            coordinates = []
 
-                        pixel_predictions = np.zeros((height, width, num_classes), dtype=np.longlong)
-                        patches = []
-                        coordinates = []
+                            for x in range(0, padded_width - patch_size, step_size):
+                                for y in range(0, padded_height - patch_size, step_size):
+                                    patch = image_tensor_padded[:, y:y + patch_size, x:x + patch_size]
+                                    patches.append(patch)
+                                    coordinates.append((x, y))
 
-                        for x in range(0, padded_width - patch_size, step_size):
-                            for y in range(0, padded_height - patch_size, step_size):
-                                patch = image_tensor_padded[:, y:y + patch_size, x:x + patch_size]
-                                patches.append(patch)
-                                coordinates.append((x, y))
+                                    if len(patches) == batch_size:
+                                        patches_tensor = torch.stack(patches).to(device)
 
-                                if len(patches) == batch_size:
-                                    patches_tensor = torch.stack(patches).to(device)
+                                        with torch.no_grad(), torch.cuda.amp.autocast():
+                                            outputs = model(patches_tensor)
 
-                                    with torch.no_grad(), torch.cuda.amp.autocast():
-                                        outputs = model(patches_tensor)
+                                        predicted_classes = torch.argmax(outputs.logits, dim=1)
 
-                                    predicted_classes = torch.argmax(outputs.logits, dim=1)
+                                        for patch_idx, (x, y) in enumerate(coordinates):
+                                            predicted_class = predicted_classes[patch_idx]
 
-                                    for patch_idx, (x, y) in enumerate(coordinates):
-                                        predicted_class = predicted_classes[patch_idx]
+                                            pixel_coords = offsets + np.array([x, y]) - padding
+                                            valid_mask = ((pixel_coords[:, 0] < width)
+                                                          & (pixel_coords[:, 1] < height)
+                                                          & (pixel_coords[:, 0] >=0)
+                                                          & (pixel_coords[:, 1] >= 0))
+                                            pixel_coords = pixel_coords[valid_mask]
+                                            pixel_predictions[pixel_coords[:, 1], pixel_coords[:, 0], predicted_class] += 1
 
-                                        pixel_coords = offsets + np.array([x, y]) - padding
-                                        valid_mask = ((pixel_coords[:, 0] < width)
-                                                      & (pixel_coords[:, 1] < height)
-                                                      & (pixel_coords[:, 0] >=0)
-                                                      & (pixel_coords[:, 1] >= 0))
-                                        pixel_coords = pixel_coords[valid_mask]
-                                        pixel_predictions[pixel_coords[:, 1], pixel_coords[:, 0], predicted_class] += 1
+                                        patches = []
+                                        coordinates = []
+                            if patches:
+                                patches_tensor = torch.stack(patches).to(device)
 
-                                    patches = []
-                                    coordinates = []
-                        if patches:
-                            patches_tensor = torch.stack(patches).to(device)
+                                with torch.no_grad(), torch.cuda.amp.autocast():
+                                    outputs = model(patches_tensor)
 
-                            with torch.no_grad(), torch.cuda.amp.autocast():
-                                outputs = model(patches_tensor)
+                                predicted_classes = torch.argmax(outputs.logits, dim=1)
 
-                            predicted_classes = torch.argmax(outputs.logits, dim=1)
+                                for patch_idx, (x, y) in enumerate(coordinates):
+                                    predicted_class = predicted_classes[patch_idx]
 
-                            for patch_idx, (x, y) in enumerate(coordinates):
-                                predicted_class = predicted_classes[patch_idx]
+                                    pixel_coords = offsets + np.array([x, y]) - padding
+                                    valid_mask = ((pixel_coords[:, 0] < width)
+                                                  & (pixel_coords[:, 1] < height)
+                                                  & (pixel_coords[:, 0] >= 0)
+                                                  & (pixel_coords[:, 1] >= 0))
+                                    pixel_coords = pixel_coords[valid_mask]
+                                    pixel_predictions[pixel_coords[:, 1], pixel_coords[:, 0], predicted_class] += 1
 
-                                pixel_coords = offsets + np.array([x, y]) - padding
-                                valid_mask = ((pixel_coords[:, 0] < width)
-                                              & (pixel_coords[:, 1] < height)
-                                              & (pixel_coords[:, 0] >= 0)
-                                              & (pixel_coords[:, 1] >= 0))
-                                pixel_coords = pixel_coords[valid_mask]
-                                pixel_predictions[pixel_coords[:, 1], pixel_coords[:, 0], predicted_class] += 1
+                            votings_per_pixel = pixel_predictions.sum(axis=2)
+                            non_zero_votings = votings_per_pixel[votings_per_pixel > 0]
+                            avg_votings_image = non_zero_votings.mean() if len(non_zero_votings) > 0 else 0
 
-                        votings_per_pixel = pixel_predictions.sum(axis=2)
-                        non_zero_votings = votings_per_pixel[votings_per_pixel > 0]
-                        avg_votings_image = non_zero_votings.mean() if len(non_zero_votings) > 0 else 0
+                            total_votings += non_zero_votings.sum()
+                            total_pixels += len(non_zero_votings)
+                            image_count += 1
 
-                        total_votings += non_zero_votings.sum()
-                        total_pixels += len(non_zero_votings)
-                        image_count += 1
+                            segmentation_map = np.argmax(pixel_predictions, axis=2)
+                            segmentation_map[votings_per_pixel == 0] = -1
 
-                        segmentation_map = np.argmax(pixel_predictions, axis=2)
-                        segmentation_map[votings_per_pixel == 0] = -1
+                            output_filename = Path(image_path).with_suffix('.png').name
+                            overlap_folder = Path(output_folder) / f'center-{central_window_size}_patch-{patch_size}_step-{int(step_size)}_pad-{int(padding)}'
+                            ###################################################################
+                            # overlap_folder DEFINED FOR 1 VOTE HERE, OTHER THAN THIS, USE THE ABOVE LINE
 
-                        output_filename = Path(image_path).with_suffix('.png').name
-                        overlap_folder = Path(output_folder) / f'center-{central_window_size}_patch-{patch_size}_step-{int(step_size)}_pad-{int(padding)}'
-                        ###################################################################
-                        # overlap_folder DEFINED FOR 1 VOTE HERE, OTHER THAN THIS, USE THE ABOVE LINE
+                            # overlap_folder = Path(output_folder) / f'{central_window_size}_{patch_size}_{int(step_size)}_oneVOTE'
+                            ###################################################################
+                            overlap_folder.mkdir(exist_ok=True, parents=True)
+                            cv2.imwrite(str(overlap_folder / output_filename), segmentation_map)
+                            print(f'Time taken: {time.perf_counter() - begin_time:.2f}s')
 
-                        # overlap_folder = Path(output_folder) / f'{central_window_size}_{patch_size}_{int(step_size)}_oneVOTE'
-                        ###################################################################
-                        overlap_folder.mkdir(exist_ok=True, parents=True)
-                        cv2.imwrite(str(overlap_folder / output_filename), segmentation_map)
-                        print(f'Time taken: {time.perf_counter() - begin_time:.2f}s')
+                    avg_votings_dataset = total_votings / total_pixels if total_pixels else 0
+                    print(f'Average votings for the entire dataset: {avg_votings_dataset:.2f}')
 
-                avg_votings_dataset = total_votings / total_pixels if total_pixels else 0
-                print(f'Average votings for the entire dataset: {avg_votings_dataset:.2f}')
+                    writer.writerow([central_window_size, patch_size, step_size, avg_votings_dataset, padding])
 
-                # writer.writerow([central_window_size, patch_size, step_size, avg_votings_dataset, padding])
-
-# print("Processing complete. Results saved to", csv_filename)
+print("Processing complete. Results saved to", csv_filename)
---
+++ lowAltitude_classification/pseudo-masks-test_SKLEARN.py
@@ -0,0 +1,122 @@
+import os
+import numpy as np
+from sklearn.metrics import f1_score, accuracy_score
+from PIL import Image
+import pandas as pd
+
+def load_image(image_path):
+    return np.array(Image.open(image_path))
+
+def calculate_metrics(pred_folder, annot_folder):
+    all_preds = []
+    all_annots = []
+
+    for filename in os.listdir(pred_folder):
+        pred_path = os.path.join(pred_folder, filename)
+        base_name = os.path.splitext(filename)[0]
+        annotation_filename = f'{base_name}-label-ground-truth-semantic.png'
+        annot_path = os.path.join(annot_folder, annotation_filename)
+        pred_image = load_image(pred_path)
+        annot_image = load_image(annot_path)
+
+        assert pred_image.shape == annot_image.shape, f"Shape mismatch: {filename}"
+
+        all_preds.extend(pred_image.flatten())
+        all_annots.extend(annot_image.flatten())
+
+    # Convert lists to numpy arrays
+    all_preds = np.array(all_preds)
+    all_annots = np.array(all_annots)
+
+    overall_f1_score = f1_score(all_annots, all_preds, average='macro')
+    pixel_accuracy = accuracy_score(all_annots, all_preds)
+
+    return overall_f1_score, pixel_accuracy
+
+
+
+
+
+
+pred_folder =  '/home/kamyar/Documents/Test_Annotated_Predictions/CENTER/52_Final_5e'
+annot_folder = '/home/kamyar/Documents/Test_Annotated_masks'
+
+results = []
+
+for subdir in os.listdir(pred_folder):
+
+
+
+    ######################################### center
+    params = subdir.split('_')
+    center = int(params[0].split('-')[1])
+    patch = int(params[1].split('-')[1])
+    step = int(params[2].split('-')[1])
+    pad = int(params[3].split('-')[1])
+
+    if pad == 184 and step == 27:
+
+        subdir_path = os.path.join(pred_folder, subdir)
+
+        overall_f1, pAcc = calculate_metrics(subdir_path, annot_folder)
+
+        print(f"Overall F1 Score: {overall_f1}")
+        print(f"Pixel Accuracy: {pAcc}")
+
+        results.append({
+            "Central Size": subdir.split('_')[0].split('-')[1],
+            "Patch Size": subdir.split('_')[1].split('-')[1],
+            "Step Size": subdir.split('_')[2].split('-')[1],
+            "Pad Size": subdir.split('_')[3].split('-')[1],
+
+            "F1": f'{overall_f1:.4f}',
+            "pAcc": f'{pAcc:.4f}',
+        })
+
+
+    ######################################## patch sizes
+    # patch = int(subdir.split('_')[0])
+    # overlap = int(subdir.split('_')[1])
+    # if overlap == 85:
+    #
+    #     subdir_path = os.path.join(pred_folder, subdir)
+    #
+    #     overall_f1, pAcc = calculate_metrics(subdir_path, annot_folder)
+    #
+    #     print(f"Overall F1 Score: {overall_f1}")
+    #     print(f"Pixel Accuracy: {pAcc}")
+    #
+    #     results.append({
+    #         "Central Size": subdir.split('_')[0].split('-')[1],
+    #         "Patch Size": subdir.split('_')[1].split('-')[1],
+    #         "Step Size": subdir.split('_')[2].split('-')[1],
+    #         "Pad Size": subdir.split('_')[3].split('-')[1],
+    #
+    #         "F1": f'{overall_f1:.4f}',
+    #         "pAcc": f'{pAcc:.4f}',
+    #     })
+
+
+    ################################ STRIDE
+    # subdir_path = os.path.join(pred_folder, subdir)
+    #
+    # overall_f1, pAcc = calculate_metrics(subdir_path, annot_folder)
+    #
+    # print(f"Overall F1 Score: {overall_f1}")
+    # print(f"Pixel Accuracy: {pAcc}")
+    #
+    # results.append({
+    #     "Central Size": subdir.split('_')[0].split('-')[1],
+    #     "Patch Size": subdir.split('_')[1].split('-')[1],
+    #     "Step Size": subdir.split('_')[2].split('-')[1],
+    #     "Pad Size": subdir.split('_')[3].split('-')[1],
+    #
+    #     "F1": f'{overall_f1:.4f}',
+    #     "pAcc": f'{pAcc:.4f}',
+    # })
+
+df = pd.DataFrame(results)
+df = df.sort_values(by=["Pad Size"])
+
+df.to_csv("lowAltitude_classification/results/New_phase_2/center/test/phase2-test-center_METRICS.csv",
+          index=False)
