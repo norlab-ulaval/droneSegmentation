d35b2ebfce1e6297ab9da2a259b878240a0af756
kamyarNsr <thekamyar.nasiri@gmail.com>
2024-12-16 16:28:22 -0500

    scaling code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-image = cv2.imread('/home/kamyar/Documents/M2F_Results/MV/2023-06-29-131242-5-Lac-Saint-Jean-4000x4000-DJI-FC7303-patch-4.png', cv2.IMREAD_GRAYSCALE)
+image = cv2.imread('/home/kamyar/Documents/M2F_Results/MV/2024-06-19-120137-13.075-ZecChapais-5280x5280-DJI-M3E-patch-3.png', cv2.IMREAD_GRAYSCALE)
 
 plt.imshow(image, cmap='gray')
 plt.axis('off')
